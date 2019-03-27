9bb42afc866d5a679094ced8d8b0816b6a266069
Vince Carollo <vincecarollo@gmail.com>
2019-03-27 09:09:04 -0600

    Finish iteration 1

---
+++ lib/character.rb
@@ -0,0 +1,11 @@
+class Character
+  attr_reader :name, :actor, :salary
+
+  def initialize(character_hash)
+    @character_hash = character_hash
+    @name = @character_hash[:name]
+    @actor = @character_hash[:actor]
+    @salary = @character_hash[:salary].to_s.delete("_").to_i
+  end
+
+end
