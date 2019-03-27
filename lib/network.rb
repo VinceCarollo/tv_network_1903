a93f0a7c77dc05802ddb1eb9fb532cb8037624db
Vince Carollo <vincecarollo@gmail.com>
2019-03-27 10:05:54 -0600

    Finish iteration 4

--- lib/network.rb
+++ lib/network.rb
@@ -4,6 +4,7 @@ class Network
   def initialize(name)
     @name = name
     @shows = []
+    @all_characters = []
   end
 
   def add_show(show)
@@ -11,12 +12,21 @@ class Network
   end
 
   def highest_paid_actor
-    all_characters = @shows.map do |show|
+    @all_characters = @shows.map do |show|
       show.characters.map do |character|
         character.character_hash
       end
     end
-    highest_paid_character = all_characters.flatten.max_by{|character| character[:salary]}
+    highest_paid_character = @all_characters.flatten.max_by{|character| character[:salary]}
     highest_paid_character[:actor]
+  end
+
+  def payroll
+    highest_paid_actor
+    payroll_data = {}
+    @all_characters.flatten.each do |character|
+      payroll_data[character[:actor]] = character[:salary]
+    end
+    payroll_data
   end
 end
